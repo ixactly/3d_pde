71a134168b0b0f5cb40f32c7c2a8a29158444787
森智希 <morisatoshinozomi@morisatoshinozominoMacBook-puro.local>
2018-09-22 19:22:47 +0900

    Have not finished

--- electric_field.py
+++ electric_field.py
@@ -1,4 +1,35 @@
 import numpy as np
-import 3d_pde as pde
+import three_d_pde as pde
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
 
-V = pde.solve_laplace_eq()
+V = pde.solve_eq()
+
+mesh = pde.CartesianGrid()
+nx = mesh.nx
+ny = mesh.ny
+nz = mesh.nz
+Y ,Z ,X = mesh.create_meshgrid()
+Ex = np.empty((nz, nx, ny))
+Ey = np.empty((nz, nx, ny))
+Ez = np.empty((nz, nx, ny))
+
+delta_x = (mesh.xmax - mesh.xmin)/nx
+delta_y = (mesh.ymax - mesh.ymin)/ny
+delta_z = (mesh.zmax - mesh.zmin)/nz
+
+for i in range(ny):
+    for j in range(nz):
+        for k in range(nx):
+            Ez[i, j, k] = -(V[i+1, j, k]-V[i, j, k])/delta_z
+            Ex[i, j, k] = -(V[i, j+1, k]-V[i, j, k])/delta_x
+            Ey[i, j, k] = -(V[i, j, k+1]-V[i, j, k])/delta_y
+
+fig = plt.figure()
+ax = Axes3D(fig)
+
+ax.quiver(X, Y, Z, Ex, Ey, Ez, color='red', normalize=False)
+ax.set_xlim([mesh.xmin, mesh.xmax])
+ax.set_ylim([mesh.ymin, mesh.ymax])
+ax.set_zlim([mesh.zmin, mesh.zmax])
+plt.show()
